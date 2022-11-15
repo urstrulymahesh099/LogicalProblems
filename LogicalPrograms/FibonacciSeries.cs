285c7e5ca9d5191125a0d7612ecff66616fb31e5
mago mahi <maheshhonnamurthy099@gmail.com>
2022-11-15 19:12:29 +0530

    [Mahesh]Add: Fibonacciseries

---
+++ LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class FibonacciSeries
+    {
+        public void Print(int num)
+        {
+            int firstNum = 0, secondNum = 1, thirdNum = 0;
+            Console.WriteLine(firstNum + "\n" + secondNum);
+            for (int i = 0; i < num; i++)
+            {
+                thirdNum = firstNum + secondNum;
+                firstNum = secondNum;
+                secondNum = thirdNum;
+                Console.WriteLine(thirdNum);
+            }
+        }
+
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LogicalPrograms;
+using System;
+namespace LogicalProblem
+
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Welcome to Logical Programming Problems");
+                Console.WriteLine("1. Fibonaccis Series" + "\n" + "2. Exit");
+                int check = Convert.ToInt32(Console.ReadLine());
+                switch (check)
+                {
+                    case 1:
+                        FibonacciSeries fibo = new FibonacciSeries();
+                        fibo.Print(10);
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
