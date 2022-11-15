d290f9e51c45af555dc64d0c44845db49a3b4425
mago mahi <maheshhonnamurthy099@gmail.com>
2022-11-15 19:21:56 +0530

    [Mahesh]Add: prime number

---
+++ LogicalPrograms/PrimeNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class PrimeNumber
+    {
+        public void CheckPrime(int num)
+        {
+            int count = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                    count++;
+            }
+            if (count == 2)
+                Console.WriteLine("Number is Prime");
+            else
+                Console.WriteLine("Number is not a Prime");
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -11,7 +11,8 @@ namespace LogicalProblem
             while (flag)
             {
                 Console.WriteLine("Welcome to Logical Programming Problems");
-                Console.WriteLine("1. Fibonaccis Series" + "\n" + "2. Check Perfect Number" + "\n" + "3. Exit");
+                Console.WriteLine("1. Fibonaccis Series" + "\n" + "2. Check Perfect Number" + "\n" + "3. PrimeNumber" + "\n" + "4. Exit");
+
                 int check = Convert.ToInt32(Console.ReadLine());
                 switch (check)
                 {
@@ -22,6 +23,10 @@ namespace LogicalProblem
                     case 2:
                         PerfectNumber enter = new PerfectNumber();
                         enter.Check(28);
+                        break;
+                    case 3:
+                        PrimeNumber pri = new PrimeNumber();
+                        pri.CheckPrime(10);
                         break;
                     default:
                         flag = false;
