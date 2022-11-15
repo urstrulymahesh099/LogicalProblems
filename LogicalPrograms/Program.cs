f76b1da14be36c6ba6fc5216c90d0bcb74a43f05
mago mahi <maheshhonnamurthy099@gmail.com>
2022-11-15 19:17:55 +0530

    [Mahesh]Add: perfect number

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -11,13 +11,17 @@ namespace LogicalProblem
             while (flag)
             {
                 Console.WriteLine("Welcome to Logical Programming Problems");
-                Console.WriteLine("1. Fibonaccis Series" + "\n" + "2. Exit");
+                Console.WriteLine("1. Fibonaccis Series" + "\n" + "2. Check Perfect Number" + "\n" + "3. Exit");
                 int check = Convert.ToInt32(Console.ReadLine());
                 switch (check)
                 {
                     case 1:
                         FibonacciSeries fibo = new FibonacciSeries();
                         fibo.Print(10);
+                        break;
+                    case 2:
+                        PerfectNumber enter = new PerfectNumber();
+                        enter.Check(28);
                         break;
                     default:
                         flag = false;
