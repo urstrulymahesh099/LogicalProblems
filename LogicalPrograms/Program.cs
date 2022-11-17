8d506c1db6833d63abeb36bcd0070fa37156d84a
mago mahi <maheshhonnamurthy099@gmail.com>
2022-11-17 19:38:33 +0530

    [Mahesh]Add: day of Week

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -11,7 +11,7 @@ namespace LogicalProblem
             while (flag)
             {
                 Console.WriteLine("Welcome to Logical Programming Problems");
-                Console.WriteLine("1. Fibonaccis Series" + "\n" + "2. Check Perfect Number" + "\n" + "3. PrimeNumber" + "\n" + "4. Reverse enter" + "\n" + "5. Coupon Number" + "\n" + "6. Temperature Conversion" + "\n" + "7. Exit");
+                Console.WriteLine("1. Fibonaccis Series" + "\n" + "2. Check Perfect Number" + "\n" + "3. PrimeNumber" + "\n" + "4. Reverse enter" + "\n" + "5. Coupon Number" + "\n" + "6. Temperature Conversion" + "\n" + "7. Day of Week" + "\n" + "8. Exit");
 
                 int check = Convert.ToInt32(Console.ReadLine());
                 switch (check)
@@ -39,6 +39,10 @@ namespace LogicalProblem
                         Temperature tem = new Temperature();
                         tem.temp1();
                         break;
+                    case 7:
+                        Day we = new Day();
+                        we.Dat();
+                        break;
                     default:
                         flag = false;
                         break;
@@ -47,3 +51,4 @@ namespace LogicalProblem
         }
     }
 }
+
