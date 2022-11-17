09d709799cf0f877a177554779bbae935c5761fb
mago mahi <maheshhonnamurthy099@gmail.com>
2022-11-17 19:27:02 +0530

    [Mahesh]Add:Temperature Conversion

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -11,7 +11,7 @@ namespace LogicalProblem
             while (flag)
             {
                 Console.WriteLine("Welcome to Logical Programming Problems");
-                Console.WriteLine("1. Fibonaccis Series" + "\n" + "2. Check Perfect Number" + "\n" + "3. PrimeNumber" + "\n" + "4. Reverse enter" + "\n" + "5. Coupon Number" + "\n" + "6. Exit");
+                Console.WriteLine("1. Fibonaccis Series" + "\n" + "2. Check Perfect Number" + "\n" + "3. PrimeNumber" + "\n" + "4. Reverse enter" + "\n" + "5. Coupon Number" + "\n" + "6. Temperature Conversion" + "\n" + "7. Exit");
 
                 int check = Convert.ToInt32(Console.ReadLine());
                 switch (check)
@@ -34,6 +34,10 @@ namespace LogicalProblem
                         break;
                     case 5:
                         CouponNumber.GenerateCoupon(10);
+                        break;
+                    case 6:
+                        Temperature tem = new Temperature();
+                        tem.temp1();
                         break;
                     default:
                         flag = false;
---
+++ LogicalPrograms/Temperature.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class Temperature
+    {
+        public void temp1()
+        {
+            double finalTemperature = 0;
+            Console.WriteLine("1. Celsius to Fahrenheit" + "\n" + "2. Fahrenheit To Celsius ");
+            int temp = Convert.ToInt32(Console.ReadLine());
+
+            switch (temp)
+            {
+                case 1:
+                    Console.WriteLine("Enter Temp in °C ");
+                    double c = Convert.ToDouble(Console.ReadLine());
+                    finalTemperature = (c * 9 / 5) + 32;
+                    Console.WriteLine(finalTemperature + "°F");
+                    break;
+                case 2:
+                    Console.WriteLine("Enter Temp in °F ");
+                    double f = Convert.ToDouble(Console.ReadLine());
+                    finalTemperature = (f - 32) * 5 / 9;
+                    Console.WriteLine(finalTemperature + "°C");
+                    break;
+            }
+        }
+
+    }
+}
